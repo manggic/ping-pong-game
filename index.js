7e9f3dffb08a872ffc7a64803e15642ca4528640
manish <manishmahto198@gmail.com>
2021-10-20 10:40:46 +0530

    changed the styling

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 let score = 0;
 
-//document.querySelector('#score').innerHTML
-
-//
-
 let targetRow = 4;
 let targetColumn = 3;
 
@@ -12,10 +8,8 @@ var column = 0;
 var keyPress = "";
 
 function callMe() {
-  
   document.addEventListener("keypress", function (e) {
     keyPress = e.key;
-    
   });
 }
 
@@ -25,7 +19,7 @@ function changeTheTarget() {
   ].innerHTML = ``;
   const randomRow = Math.floor(Math.random() * 15) + 1;
   const randomCol = Math.floor(Math.random() * 27) + 1;
-  
+
   targetRow = randomRow;
   targetColumn = randomCol;
   document.getElementsByTagName("tr")[randomRow].children[
@@ -36,13 +30,10 @@ function changeTheTarget() {
 }
 
 function ifCallZThen() {
-  
-
   keyPress = "";
   let j = row;
   function loop2() {
     if (row === targetRow && column === targetColumn) {
-      
       changeTheTarget();
     }
 
@@ -60,19 +51,17 @@ function ifCallZThen() {
     setTimeout(function () {
       document.getElementsByTagName("tr")[j].children[column].innerHTML = ``;
 
-      if(j < 15){
+      if (j < 15) {
         document.getElementsByTagName("tr")[j + 1].children[
           column
         ].innerHTML = `<div id='source'></div>`;
       }
-      
+
       j++;
       row = j;
       if (j <= 15) {
-        
         loop2();
       } else {
-        
         document.querySelector("#btn").disabled = false;
 
         document.getElementById("over").innerHTML = "GAME OVER";
@@ -87,13 +76,10 @@ function ifCallZThen() {
   loop2();
 }
 function ifCallWThen() {
-  
-
   keyPress = "";
   let j = row;
   function loop2() {
     if (row === targetRow && column === targetColumn) {
-      
       changeTheTarget();
     }
     if (keyPress === "z" || keyPress === "z") {
@@ -109,19 +95,17 @@ function ifCallWThen() {
     setTimeout(function () {
       document.getElementsByTagName("tr")[j].children[column].innerHTML = ``;
 
-      if(j > 0){
+      if (j > 0) {
         document.getElementsByTagName("tr")[j - 1].children[
           column
         ].innerHTML = `<div id='source'></div>`;
       }
-      
+
       j--;
       row = j;
       if (j >= 0) {
-        
         loop2();
       } else {
-        
         document.querySelector("#btn").disabled = false;
         document.getElementById("over").innerHTML = "GAME OVER";
         // document.getElementsByTagName("tr")[row - 1].children[
@@ -135,13 +119,10 @@ function ifCallWThen() {
   loop2();
 }
 function ifCallAThen() {
-  
-
   keyPress = "";
   let j = column;
   function loop2() {
     if (row === targetRow && column === targetColumn) {
-      
       changeTheTarget();
     }
     if (keyPress === "w" || keyPress === "W") {
@@ -156,19 +137,17 @@ function ifCallAThen() {
     }
     setTimeout(function () {
       document.getElementsByTagName("tr")[row].children[j].innerHTML = ``;
-      if( j>0 ){
+      if (j > 0) {
         document.getElementsByTagName("tr")[row].children[
           j - 1
         ].innerHTML = `<div id='source'></div>`;
       }
-      
+
       j--;
       column = j;
       if (j >= 0) {
-        
         loop2();
       } else {
-        
         document.querySelector("#btn").disabled = false;
         document.getElementById("over").innerHTML = "GAME OVER";
         // document.getElementsByTagName("tr")[row].children[
@@ -183,13 +162,11 @@ function ifCallAThen() {
   loop2();
 }
 function ifCallLThen() {
-  
   keyPress = "";
   let j = column;
   function loop2() {
     setTimeout(function () {
       if (row === targetRow && column === targetColumn) {
-        
         changeTheTarget();
       }
       if (keyPress === "w" || keyPress === "W") {
@@ -203,19 +180,17 @@ function ifCallLThen() {
         return;
       }
       document.getElementsByTagName("tr")[row].children[j].innerHTML = ``;
-      if(j < 27 ){
+      if (j < 27) {
         document.getElementsByTagName("tr")[row].children[
           j + 1
         ].innerHTML = `<div id='source'></div>`;
       }
-      
+
       j++;
       column = j;
       if (j <= 27) {
-        
         loop2();
       } else {
-        
         document.querySelector("#btn").disabled = false;
         document.getElementById("over").innerHTML = "GAME OVER";
         // document.getElementsByTagName("tr")[row].children[
@@ -253,25 +228,15 @@ function start() {
   let rowOne = document.getElementsByTagName("tr")[0].children;
   var i = 1;
 
-  
-
   function myLoop() {
     column = i - 1;
     if (keyPress === "z" || keyPress === "Z") {
-      
-
       ifCallZThen();
     } else if (keyPress === "w" || keyPress === "W") {
-      
-
       ifCallWThen();
     } else if (keyPress === "a" || keyPress === "A") {
-      
-
       ifCallAThen();
     } else if (keyPress === "l" || keyPress === "L") {
-      
-
       ifCallLThen();
     } else {
       setTimeout(function () {
@@ -282,8 +247,6 @@ function start() {
         if (i < 28) {
           myLoop();
         } else {
-          
-
           document.querySelector("#btn").disabled = false;
           document.getElementById("over").innerHTML = "GAME OVER";
           document.getElementsByTagName("tr")[row].children[
@@ -295,6 +258,4 @@ function start() {
   }
 
   myLoop();
-
-  
 }
